7643ae21fc75e257837e3eac701d0e1c5596b03b
Wesley Simms <wesley93@ksu.com>
2025-04-07 15:37:57 -0500

    Added Some style

--- images/urls.py
+++ images/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.image_list, name='image_list'),
     path('download/<str:filename>/', views.download_image, name='download_image'),  # Download page
     path('folders/', views.folder_list, name='folder_list'),
-    path('folders/<str:folder_name>/', views.image_list_folder, name='image_list'),
+    path('folders/<str:folder_name>/', views.image_list_folder, name='folder_image_list'),
     path('download_folder/<str:filename>/', views.download_image_folder, name='folder_download')
 ]
