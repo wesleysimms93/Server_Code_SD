ce8bc7cdd31bc68fbc60cc9dc640b8fb38fa0400
wesleysimms93 <wesley93@ksu.edu>
2025-04-26 13:26:21 -0500

    Added some edits

--- controls/urls.py
+++ controls/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('download-image/', views.download_from_external, name='download_from_external'),
     path('video/', views.video_stream, name='video_stream'),
     path('Manual/execute/<str:action>/', views.execute_cool_script, name='execute_cool_script'),
+    path('execute_cool_script/<str:action>/', views.execute_cool_script, name='execute_cool_script'),
 ]
